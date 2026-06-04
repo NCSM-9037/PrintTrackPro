63db8bd3eab3e98ef7abcd1dc014021d631704b0
NCSM-9037 <ncsm6724@gmail.com>
2026-06-04 11:44:09 +0530

    Add global exception logging to app

--- desktop/App.xaml.cs
+++ desktop/App.xaml.cs
@@ -1,4 +1,4 @@
-﻿using System.Configuration;
+using System.Configuration;
 using System.Data;
 using System.Windows;
 
@@ -9,5 +9,22 @@ namespace PrintTrackPro.Desktop;
 /// </summary>
 public partial class App : Application
 {
+    protected override void OnStartup(StartupEventArgs e)
+    {
+        base.OnStartup(e);
+        
+        AppDomain.CurrentDomain.UnhandledException += (s, args) =>
+        {
+            System.IO.File.WriteAllText("crash_log.txt", args.ExceptionObject.ToString());
+            MessageBox.Show("Fatal Error: " + args.ExceptionObject.ToString());
+        };
+        
+        this.DispatcherUnhandledException += (s, args) =>
+        {
+            System.IO.File.WriteAllText("crash_log.txt", args.Exception.ToString());
+            MessageBox.Show("UI Error: " + args.Exception.ToString());
+            args.Handled = true;
+        };
+    }
 }
 
